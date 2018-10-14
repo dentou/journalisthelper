cf7d90d39c13f4fc391e62a2f144abdb714ec429
dentou <trantienhuy.phys@gmail.com>
2018-10-14 09:45:51 +0700

    bugs fix

--- src/embedded_chart.js
+++ src/embedded_chart.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const MAX_DATA_POINTS = 5;
+const EVENT_SOURCE = '//localhost/journalisthelper/test/dataserver.php';
 
 let data = {
     labels: [],
@@ -42,7 +43,7 @@ drawChart()
 //     data: data
 // });
 
-let eventSource = new EventSource('//localhost/journalisthelper/modules/dataserver.php');
+let eventSource = new EventSource(EVENT_SOURCE);
 console.log(eventSource.withCredentials);
 console.log(eventSource.readyState);
 console.log(eventSource.url);
@@ -66,14 +67,16 @@ eventSource.addEventListener("update", function (event) {
 })
 
 eventSource.addEventListener("init", function (event) {
+    console.log(event.data);
     let jdata = JSON.parse(event.data);
-
     dataChart.destroy();
+    createDataset(jdata.dataType)
     dataChart = Chart.Line("myChart", {
         options: options,
         data: data
     });
-    let labels = jdata.x;
+    // removeDataset(dataChart, 0);
+    let labels  = jdata.x;
     let values = jdata.y;
     let i;
     for (i = 0; i < labels.length; i++) {
@@ -114,3 +117,27 @@ function removeData(chart, dataSetIndex, removalIndex) {
         chart.update();
     }
 }
+
+function removeDataset(chart, removalIndex) {
+    if(removalIndex >= 0) { //make sure this element exists in the array
+        data.datasets.splice(removalIndex, 1);
+    }
+}
+
+
+
+function createDataset(datasetLabel) {
+    data = {
+        labels: [],
+        datasets: [{
+            label: datasetLabel,
+            backgroundColor: "rgba(255,99,132,0.2)",
+            borderColor: "rgba(255,99,132,1)",
+            borderWidth: 2,
+            hoverBackgroundColor: "rgba(255,99,132,0.4)",
+            hoverBorderColor: "rgba(255,99,132,1)",
+            data: [],
+        }]
+    };
+}
+
